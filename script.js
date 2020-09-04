0540088ae4d8cbf40f190c66bfef787b7c26e18f
Shravani Allam Reddy <shravani.allam@gmail.com>
2020-09-04 11:49:47 -0600

    added code to check if answer is correct or not and print it below

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 
 const timeEl = document.querySelector(".time");
 const questionEl = document.querySelector("#question");
-const button1El = document.querySelector("#button1");
-const button2El = document.querySelector("#button2");
-const button3El = document.querySelector("#button3");
-const button4El = document.querySelector("#button4");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
 const quizEl = document.querySelector("#quiz");
 const mainEl = document.querySelector("#main");
 const startEl = document.querySelector("#start");
-
+const buttonEl = document.querySelector("#button-container");
+const correct = document.querySelector("#correct");
+const wrong = document.querySelector("#wrong");
+// quizEl.innerHTML = "<p>Hello World</p>";
 const questionAnswers = [
     {
         question: "Inside which HTML element do we put the JavaScript?",
@@ -39,9 +42,18 @@ const questionAnswers = [
             "ESLint"
         ],
         correctAnswer: "ESLint"
+    },
+    {
+        question: "How can you get the type of arguments passed to a function?",
+        answers: [
+            "using typeof operator",
+            "using getType function",
+            "Both of the above",
+            "None of the above"
+        ],
+        correctAnswer: "using typeof operator"
     }
 ];
-
 
 let secondsLeft = 60;
 
@@ -50,17 +62,12 @@ function startTimer() {
     let timerInterval = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = secondsLeft;
-
         if (secondsLeft === 0) {
             clearInterval(timerInterval);//cancel the timer 
-
         }
 
     }, 1000);
-
 }
-
-startTimer();
 
 
 function startQuiz(event) {
@@ -70,17 +77,113 @@ function startQuiz(event) {
 }
 
 
-
-// for(let i = 0 ; i < questions.length ; i++){
+let index = 0;
 let currentQuestion = questionAnswers[0];
-
-questionEl.textContent = currentQuestion.question;
-button1El.textContent = currentQuestion.answers[0];
-button2El.textContent = currentQuestion.answers[1];
-button3El.textContent = currentQuestion.answers[2];
-button4El.textContent = currentQuestion.answers[3];
+nextQuestion();
 
 
+function nextQuestion() {
+    if (index < questionAnswers.length) {
+        currentQuestion = questionAnswers[index];
+        questionEl.textContent = currentQuestion.question;
+        button1.textContent = currentQuestion.answers[0];
+        button2.textContent = currentQuestion.answers[1];
+        button3.textContent = currentQuestion.answers[2];
+        button4.textContent = currentQuestion.answers[3];
+    }
+    else {
+      //local storage -optional
+      //view highscores
+
+    }
+}
+
+// button1.addEventListener("click", function (event) {
+//     event.preventDefault();
+
+//     if (button1.textContent === currentQuestion.correctAnswer) {
+
+//         alert("correct");
+//     }
+//     else {
+//         alert("wrong");
+//     } 
+//     index++;
+//     nextQuestion();
+// });
+
+// button2.addEventListener("click", function (event) {
+//     event.preventDefault();
+
+//     if (button2.textContent === currentQuestion.correctAnswer) {
+
+//         alert("correct");
+//     }
+//     else {
+//         alert("wrong");
+//     }
+
+//     index++;
+//     nextQuestion();
+// });
+
+// button3.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     if (button3.textContent === currentQuestion.correctAnswer) {
+
+//         alert("correct");
+//     }
+//     else {
+//         alert("wrong");
+//     }
+
+//     index++;
+//     console.log(index);
+//     nextQuestion();
+// });
+
+// button4.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     if (button4.textContent === currentQuestion.correctAnswer) {
+
+//         // let pEl = document.createElement("p");
+//         // pEl.textContent = "Correct Answer";
+//         // quizEl.appendChild(pEl);
+
+//         alert("correct");
+//     }
+//     else {
+//         alert("wrong");
+//     }
+
+//     index++;
+//     console.log(index);
+//     nextQuestion();
+// });
+
+buttonEl.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log(event.target);
+    console.log(event.target.textContent);
+
+    if (event.target.textContent === currentQuestion.correctAnswer) {
+        correct.style.display = "block";
+    }
+    else {
+        wrong.style.display = "block";
+        secondsLeft = secondsLeft - 10;
+        timeEl.textContent = secondsLeft;
+    }
+
+    setTimeout(function () {
+        index++;
+        nextQuestion();
+        correct.style.display = "none";
+        wrong.style.display = "none";
+    }, 2000)
+
+});
 
 
 startEl.addEventListener("click", startQuiz);
+startEl.addEventListener("click", startTimer);
